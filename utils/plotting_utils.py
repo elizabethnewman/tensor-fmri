e25412d0a9b22bea27563a23639e6ea41692cb08
elizabethnewman <elizabeth.newman@emory.edu>
2021-05-28 00:46:55 -0400

    include title in plots

--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -65,8 +65,7 @@ def slice_subplots(A, axis=-1, num_slices=25, title='', num_col=None):
             if count >= A.shape[-1]:
                 break
 
-
-    # fig.suptitle(title)
+    fig.suptitle(title)
     # plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
     # cax = plt.axes([0.85, 0.1, 0.075, 0.8])
     # plt.colorbar(cax=cax)
