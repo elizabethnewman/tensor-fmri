4c17fc8efe2f04dbb8c3fb45834420ffbbce15e8
elizabethnewman <elizabeth.newman@emory.edu>
2021-06-15 10:52:23 -0400

    big reorg!

---
+++ ex_starplus_local_tsvd.py
@@ -0,0 +1,98 @@
+# import
+import numpy as np
+import tensor.tensor_product_wrapper as tp
+from utils.plotting_utils import montage_array, slice_subplots
+import matplotlib.pyplot as plt
+import similarity_metrics as sm
+
+
+# ==================================================================================================================== #
+# choose product type {'f', 't', 'c'}
+prod_type = 't'
+
+
+# ==================================================================================================================== #
+# define projection
+def projection(A, U, prod_type):
+    training_coeff = tp.ten_prod(tp.ten_tran(U, prod_type=prod_type), A, prod_type=prod_type)
+    return tp.ten_prod(U, training_coeff, prod_type=prod_type)
+
+
+# ==================================================================================================================== #
+# for reproducibility
+np.random.seed(20)
+
+
+# load data
+# we need the variables
+#   training_data, training_labels, test_data, test_labels, num_classes
+
+
+# visualize data
+for i in range(num_classes):
+    slice_subplots(test_data[:, test_labels == i, :], axis=1, title='class ' + str(i))
+    plt.show()
+
+print(test_data.shape)
+print(test_labels)
+
+# ==================================================================================================================== #
+# form local t-svd
+num_classes = len(np.unique(training_labels))
+k = 2
+
+U = []
+for i in range(num_classes):
+    u, _, _, _ = tp.ten_svd(training_data[:, training_labels == i, :], k, prod_type=prod_type)
+    U.append(u)
+
+# visualizations
+for i in range(num_classes):
+    slice_subplots(U[i], axis=1, title='basis elments for class ' + str(i))
+    plt.show()
+
+
+# ==================================================================================================================== #
+# compute results on training and test data
+training_error = np.zeros([num_classes, training_data.shape[1]])
+test_error = np.zeros([num_classes, test_data.shape[1]])
+for i in range(num_classes):
+    training_projection = projection(training_data, U[i], prod_type=prod_type)
+    training_error[i, :] = sm.frobenius_metric(training_data, training_projection, axis=1)
+
+    test_projection = projection(test_data, U[i], prod_type=prod_type)
+    test_error[i, :] = sm.frobenius_metric(test_data, test_projection, axis=1)
+
+
+# classification
+training_predicted_classes = np.argmin(training_error, axis=0).reshape(-1)
+test_predicted_classes = np.argmin(test_error, axis=0).reshape(-1)
+
+# results
+training_num_correct = np.sum(training_predicted_classes == training_labels)
+training_accuracy = training_num_correct / training_data.shape[1]
+
+test_num_correct = np.sum(test_predicted_classes == test_labels)
+test_accuracy = test_num_correct / test_data.shape[1]
+
+print('train accuracy = %0.2f' % (100 * training_accuracy))
+print('test accuracy = %0.2f' % (100 * test_accuracy))
+
+# plot results
+plt.figure()
+for i in range(num_classes):
+    plt.semilogy(training_error[i, :], 'o', label=i)
+
+plt.xlabel('image index')
+plt.ylabel('distance score (lower is better)')
+plt.legend()
+plt.show()
+
+
+# ==================================================================================================================== #
+# visualizations of projected tensors
+for j in range(num_classes):
+    for i in range(num_classes):
+        A = projection(training_data[:, training_labels == j, :], U[i], prod_type=prod_type)
+        slice_subplots(A[:, :4, :], axis=1, title='projection of class ' + str(j) + ' onto basis for class ' + str(i))
+        plt.show()
